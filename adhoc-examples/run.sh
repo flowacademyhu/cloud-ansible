1bb919853923ecb02c08347df355dcbe1c509e54
Tóth László <you@example.com>
2022-01-10 19:10:15 +0100

    delete unused tasks

--- adhoc-examples/run.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-ansible \
-    all \
-    --inventory db:22, \
-    --module-name raw \
-    --args 'cat /etc/hostname' \
-    --user vagrant \
-    --private-key '/home/vagrant/.ssh/id_rsa'
--- adhoc-with-inventory/run.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-ansible \
-    remotes \
-    --inventory inventory.yml \
-    --module-name raw \
-    --args 'cat /etc/hostname'
