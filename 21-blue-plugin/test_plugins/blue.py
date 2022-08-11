57a1b2b1820d4b10d1629fe827bd4c92749cec38
Tóth László <you@example.com>
2022-08-11 11:12:09 +0200

    add blue plugin without collection

---
+++ 21-blue-plugin/test_plugins/blue.py
@@ -0,0 +1,23 @@
+# Ansible custom blue test plugin
+
+def is_blue(string):
+    blue_values = [
+        'blue',
+        'kek',
+        '#0000ff',
+        '#00f',
+        'rgb(0,0,255)',
+        'rgb(0%,0%,255%)'
+    ]
+    if string in blue_values:
+        return True
+    else:
+        return False
+
+class TestModule(object):
+    ''' Ansible core blue tests '''
+
+    def tests(self):
+        return {
+            'blue': is_blue
+        }
